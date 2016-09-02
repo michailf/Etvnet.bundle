033ecf2cf5b396bd1233dd100ce1b20d85ea4ea1
Alexander Shvets <alexander.shvets@gmail.com>
2016-09-01 20:51:46 -0400

    sync code

--- test/etvnet_service_test.py
+++ test/etvnet_service_test.py
@@ -124,7 +124,7 @@ class EtvnetServiceTest(unittest.TestCase):
         self.assertNotEqual(len(result['data']), 0)
 
     def test_get_url(self):
-        id = '946671' # 329678
+        id = '580162' # 329678
         bitrate = '1200'
         format = 'mp4'
 
@@ -257,6 +257,72 @@ class EtvnetServiceTest(unittest.TestCase):
 
     def test_get_video_resolution(self):
         print self.service.bitrate_to_resolution(1500)
+
+    def test_live_channels(self):
+        result = self.service.get_live_channels()
+
+        #print(json.dumps(result, indent=4))
+
+        self.assertEqual(result['status_code'], 200)
+        self.assertNotEqual(len(result['data']), 0)
+
+        for value in result['data']:
+            print(value['id'])
+            print(value['name'])
+            print(value['icon'])
+            print(value['live_format'])
+            print(value['files'])
+
+    def test_live_channel(self):
+        result = self.service.get_live_channels()
+
+        #print(json.dumps(result, indent=4))
+
+        self.assertEqual(result['status_code'], 200)
+        self.assertNotEqual(len(result['data']), 0)
+
+        channel = result['data'][0]
+
+        url_data = self.service.get_url(None, channel_id=channel['id'], bitrate='400', format='mp4', live=True, offset=channel['offset'])
+
+        print(url_data)
+
+        self.assertNotEqual(url_data['url'], None)
+
+    def test_live_schedule(self):
+        result = self.service.get_live_channels()
+        channel = result['data'][0]
+
+        result = self.service.get_live_schedule(live_channel_id=channel['id'])
+
+        #print(json.dumps(result, indent=4))
+
+        for value in result['data']:
+            print(value['rating'])
+            print(value['media_id'])
+            print(value['name'])
+            print(value['finish_time'])
+            print(value['start_time'])
+            print(value['current_show'])
+            print(value['categories'])
+            print(value['efir_week'])
+            print(value['channel'])
+            print(value['description'])
+
+        self.assertEqual(result['status_code'], 200)
+        self.assertNotEqual(len(result['data']), 0)
+
+    def test_live_categories(self):
+        result = self.service.get_live_categories()
+
+        #print(json.dumps(result, indent=4))
+
+        for value in result['data']:
+            print(value['id'])
+            print(value['name'])
+
+        self.assertEqual(result['status_code'], 200)
+        self.assertNotEqual(len(result['data']), 0)
 
 if __name__ == '__main__':
     unittest.main()
